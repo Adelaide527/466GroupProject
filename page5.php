6cac752edc7c84ecd9f6dd2ab2172c18f07edf2e
Z1871561 <70650295+Z1871561@users.noreply.github.com>
2020-11-29 13:41:54 -0600

    Add files via upload

---
+++ page5.php
@@ -0,0 +1,48 @@
+<html><head><title>Workouts</title></head><body>
+<?php
+
+include('secrets.php');
+
+
+try{
+        //Connects to Dadabase
+  $dsn = "mysql:host=courses;dbname=z1871561";
+  $pdo = new PDO($dsn, $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+  echo"<header>";
+  echo"<h1>Enter Your Workout</h1>";
+  echo"</header>";
+
+  echo"<form action='http://students.cs.niu.edu/~z1871561/AddPart.php' method='GET'>";
+
+  //Holds the ID
+  echo"Enter your ID:<input type='text' name='ID'/><br>";
+  //Holds Workout
+  echo"Workout Type:<input type='text' name='Type'/><br>";
+  //Holds Intensity
+  echo"Intensity:<select name='Intensity'>";
+  echo"<option value='Low'>Low</option>";
+  echo"<option value='Medium'>Medium</option>";
+  echo"<option value='High'>High</option>";
+  echo"</select><br>";
+  //Holds Duration
+  echo"Duration:<input type='text' name='Duration'/><br>";
+  //Holds the Calories Burnt
+  echo"Calories Burned:<input type='text' name='Calories'/><br>";
+  //Holds the Date
+  echo"Date:<input type='date' name='Date'/><br>";
+
+  //Creates a button to submit
+  echo"<input type='submit' name='submit'/>\n";
+  echo"</form>";
+
+
+
+}
+catch(PDOexception $e) {
+  echo "Connection to database failed: " . $e->getMessage();
+}
+
+?>
+</body></html>
