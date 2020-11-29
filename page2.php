b1891d39eed6c8de5656159f26a7190d7da0c4e1
Ryan1597 <43765660+Ryan1597@users.noreply.github.com>
2020-11-29 09:14:09 -0600

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,18 @@
+<?php
+#Page 2
+?>
+
+<html>
+
+<h3>Update your weight</h3>
+
+<body>
+
+<form method="POST" >
+	<label for="weight">Enter your new weight: </label>
+	<input type="number" id="weight" name="weight">lbs</input>
+	<input type="submit" value="Update">
+</form>
+
+</body>
+</html>
